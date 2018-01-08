427ab7544730443fb6ec3aeb4ce8bc757f96c682
chatodesouza <ccagdaskosee@gmail.com>
2018-01-09 01:45:12 +0300

    First commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD4ypTpbN2P-HdjlRBPYlyrBIegfFXcmEc",
+    authDomain: "shoppinglist-af9f5.firebaseapp.com",
+    databaseURL: "https://shoppinglist-af9f5.firebaseio.com",
+    projectId: "shoppinglist-af9f5",
+    storageBucket: "",
+    messagingSenderId: "731146545992"
+};
